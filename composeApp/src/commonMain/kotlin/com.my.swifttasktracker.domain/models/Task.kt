266bed240581f8766e93ae7f6cd297b38e81dd73
AlexMy <alexmy@outlook.com>
2024-04-29 19:47:39 +0500

    STT-18 Migration to Shared UI

--- composeApp/src/commonMain/kotlin/com.my.swifttasktracker.domain/models/Task.kt
+++ composeApp/src/commonMain/kotlin/com.my.swifttasktracker.domain/models/Task.kt
@@ -5,5 +5,6 @@ import kotlinx.datetime.LocalDateTime
 class Task(
     val name: String,
     val createdDate: LocalDateTime,
-    val updatedDate: LocalDateTime
+    val updatedDate: LocalDateTime,
+    val isDeleted: Boolean
 )
